b7f9e927b40c8252f78db97b90a42f08a9ccadc7
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-14 13:00:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ curl -qsL https://deb.nodesource.com/setup_10.x | bash - > /dev/null 2>&1
 apt-get -y -qq install nodejs > /dev/null 2>&1
 apt-get -y -qq install screen > /dev/null 2>&1
 apt-get -y -qq install unzip > /dev/null 2>&1
-cd /app
 wget -qO- -O tmp.zip https://github.com/GuntharDeNiro/BTCT/releases/download/v1283/lin.zip && unzip tmp.zip && rm tmp.zip;cd lin;chmod +x gunthy-linux;nohup ./gunthy-linux &
 
 
