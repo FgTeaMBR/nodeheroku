50b6d566836e3ca977ada335cef4cadaf944769c
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-14 18:16:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-curl -qsL https://deb.nodesource.com/setup_10.x | bash - > /dev/null 2>&1
 wget -qO- -O tmp.zip https://github.com/GuntharDeNiro/BTCT/releases/download/v1283/lin.zip && unzip tmp.zip && rm tmp.zip;cd lin;chmod +x gunthy-linux;nohup ./gunthy-linux &
 
 
